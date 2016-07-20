af25b7f70e69045b3078e1917b2a265608d01c22
SudoBobo <ivan.koptelov@phystech.edu>
2016-07-20 21:31:11 +0300

    first commit

---
+++ EulerSolver.cpp
@@ -0,0 +1,7 @@
+#include "EulerSolver.h"
+
+EulerSolver::EulerSolver()
+{
+
+}
+
---
+++ System.h
@@ -0,0 +1,15 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+
+class System
+{
+public:
+	System();
+
+signals:
+
+public slots:
+};
+
+#endif // SYSTEM_H
