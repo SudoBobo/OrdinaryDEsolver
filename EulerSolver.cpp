86e9b3f4fb0ffcdca293b4b106d9c87bcbba1f11
SudoBobo <ivan.koptelov@phystech.edu>
2016-07-22 21:13:53 +0300

    hhh

--- EulerSolver.cpp
+++ EulerSolver.cpp
@@ -1,7 +1,19 @@
 #include "EulerSolver.h"
 
-EulerSolver::EulerSolver()
+EulerSolver::EulerSolver(const System & system, State & state,
+						 double a, double b, double h, double yInitial) :
+						Solver(system, state, a, b, h, yInitial)
 {
 
 }
+
+void EulerSolver::solve()
+{
+
+	state(0, 0, currentPosition + 1) = (state(0, 0, currentPosition) +
+				 h * system.computeDerivative((a + double(currentPosition) * h),
+											  state(0, 0, currentPosition)));
+	  currentPosition++;
+}
+
 
